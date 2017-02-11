41062275ba1fe7bbf852828fb49ea934846b3dc2
Edwin Chan <edwinchan@u.nus.edu>
2017-02-11 19:13:03 +0800

    base commits still

--- slave/analyse_input.py
+++ slave/analyse_input.py
@@ -3,8 +3,25 @@ import argparse
 
 from keras.models import *
 
+
+def execute_main(input, model, training):
+    model = load_model(model)
+    X_fb, X_hc, X_ug_ X = prep_input_samples()
+    model.predict(input)
+
+
 def main(paths):
-    model = load_model(paths['input'])
+    input, model, training = load_references(paths)
+    execute_main(input,model,training)
+
+
+def load_references(paths):
+    paths = vars(paths)
+    input = load_model(paths['input'])
+    model = load_model(paths['model'])
+    training = load_model(paths['training'])
+
+
 
 def prep_input_samples(array_sizes, x_training_data):
     count = 0
@@ -18,11 +35,12 @@ def prep_input_samples(array_sizes, x_training_data):
     count += array_sizes[3]
     X_st = np.array(map(lambda x: x[count:count + array_sizes[4]], x_training_data))
     count += array_sizes[4]
-    return X_fb, X_hc, X_pindel, X_st, X_ug
+    return X_fb, X_hc, X_ug, X_pindel, X_st
 
 if __name__ == "__main__":
     start_time = time.time()
     parser = argparse.ArgumentParser(description="train neural net")
+        parser.add_argument('-t', '--training', help="give directories with files")
     parser.add_argument('-m', '--model', help="give directories with files")
     parser.add_argument('-i', '--input', help="give directories with files")
     input_path = parser.parse_args()
